d0e472953c5aa4d2bf2384ff3fd59d0f3baf9f8f
Nguyễn Văn Minh <nguyenvanminhkma@gmail.com>
2018-01-02 20:35:03 +0700

    Update controller.py

--- controller.py
+++ controller.py
@@ -62,6 +62,14 @@ def handle(msg):
             cmd = str(args[1])
             output = os.popen(cmd).read()
             bot.sendMessage(chat_id, output)
+        elif command == "/install":
+            package = str(args[1])
+            os.system("apt-get -y install "+ package)
+            output = os.popen("dpkg-query -W "+ package").read().split()
+                if str(output[0]) == package:
+                    bot.sendMessage(chat_id, "install success")
+                else:
+                    bot.sendMessage(chat_id, "no packages found matching "+ package)
         else:
             output = 'Sorry, we do not still support your command'
             bot.sendMessage(chat_id, output)
